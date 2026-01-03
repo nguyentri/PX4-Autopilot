0b7945df4f760f03cfbb05e8983392ea5d07b1f7
nguyentri <trongtribk06@gmail.com>
2026-01-03 12:39:59 +0700

    px4/ra8p1: fix SPI_SPEED of ICM to match with the default spi ra freq

--- src/drivers/imu/invensense/icm20948/InvenSense_ICM20948_registers.hpp
+++ src/drivers/imu/invensense/icm20948/InvenSense_ICM20948_registers.hpp
@@ -58,7 +58,7 @@ static constexpr uint8_t Bit7 = (1 << 7);
 static constexpr uint32_t I2C_SPEED = 100 * 1000; // 100 kHz I2C serial interface
 static constexpr uint8_t I2C_ADDRESS_DEFAULT = 0x69; // 0b1101001
 
-static constexpr uint32_t SPI_SPEED = 7 * 1000 * 1000; // 7 MHz SPI
+static constexpr uint32_t SPI_SPEED = 1 * 1000 * 1000; // 1 MHz SPI
 static constexpr uint8_t DIR_READ = 0x80;
 
 static constexpr uint8_t WHOAMI = 0xEA;
